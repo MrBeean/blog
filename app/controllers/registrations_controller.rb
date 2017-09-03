eaeef8f2d7b6fe427f647e8f7eb690ee413fa5c6
mrbeean_HP <MrSitiZ@yandex.ru>
2017-09-03 18:53:26 +0300

    add views for devise, correct registrations routers

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    flash[:info] = 'Registrations are not open yet, but you can write me a letter'
+    redirect_to root_path
+  end
+
+  def create
+    flash[:info] = 'Registrations are not open yet, but you can write me a letter'
+    redirect_to root_path
+  end
+end
