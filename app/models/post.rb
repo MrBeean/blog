5cfa614022ad37f1b7124bf634567a616676abda
mrbeean_HP <MrSitiZ@yandex.ru>
2017-09-17 01:50:24 +0300

    add main img to posts, fix img responsive fore posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
+  before_save do
+    prepeare_post(self.post)
+  end
+
   validates :title, presence: true, length: {maximum: 255}
   validates :post, presence: true
   validates :user, presence: true
@@ -10,4 +14,10 @@ class Post < ActiveRecord::Base
   end
 
   mount_uploader :mainimg, MainimgUploader
+
+  private
+
+  def prepeare_post(post)
+    post.gsub!(/<img alt=""/, '<img class="img-responsive" alt=""')
+  end
 end
