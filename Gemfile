d7a4959e760af7eb222e2f81232784745e1be01a
mrbeean_HP <MrSitiZ@yandex.ru>
2017-09-22 23:32:41 +0300

    schema update & gemfile

--- Gemfile
+++ Gemfile
@@ -11,12 +11,10 @@ gem 'figaro'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 gem 'impressionist'
-
 # Use postgresql as the database for Active Record
 gem 'pg'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 4.2.0'
-
 gem 'rails-i18n', '~> 4.0', '>= 4.0.8'
 gem 'rmagick'
 # Use SCSS for stylesheets
@@ -24,8 +22,6 @@ gem 'sass-rails'
 gem 'simple_form'
 gem 'therubyracer'
 gem 'twitter-bootstrap-rails', '~> 3.2'
-
-
 gem 'will_paginate', '~> 3.1.1'
 gem 'will_paginate-bootstrap'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 20170922091911) do
   add_index "impressions", ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index", using: :btree
   add_index "impressions", ["user_id"], name: "index_impressions_on_user_id", using: :btree
 
+  create_table "photos", force: :cascade do |t|
+    t.string   "photo"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "photos", ["post_id"], name: "index_photos_on_post_id", using: :btree
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "post"
@@ -76,5 +85,6 @@ ActiveRecord::Schema.define(version: 20170922091911) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "photos", "posts"
   add_foreign_key "posts", "users"
 end
