12f7d7e35a1ce249fdcc0662c427fc661f809ff0
mrbeean_HP <MrSitiZ@yandex.ru>
2017-09-28 22:29:41 +0300

    delete all rspec tests, remove all tests gems

--- spec/factories/posts.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :post, class: Post do
-    title { Faker::Job.title }
-    post { Faker::Lorem.paragraphs }
-    visible { Faker::Boolean.boolean }
-
-    association :user
-  end
-end
--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :user, class: User do
-    name { Faker::StarWars.character }
-    email { Faker::Internet.safe_email }
-    info { Faker::Lorem.sentences(1) }
-
-    after(:build) { |user| user.password_confirmation = user.password = 'testtest' }
-  end
-end
